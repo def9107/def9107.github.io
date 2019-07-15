0c7f8bfd1444e14f6ae8d667e1552fe356becc62
def9107 <50330680+def9107@users.noreply.github.com>
2019-07-15 22:20:54 +0300

    Update main.js

--- Tajem/main.js
+++ Tajem/main.js
@@ -38,5 +38,7 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    video.pause();
+    video.currentTime = 0;  
   }
 }
