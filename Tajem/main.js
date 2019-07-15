69b33843fe1de986924d2ffd2eaeeea9094ae48f
def9107 <50330680+def9107@users.noreply.github.com>
2019-07-15 22:17:31 +0300

    добавлено модальное окно с видео

---
+++ Tajem/main.js
@@ -0,0 +1,42 @@
+//слайд-шоу хедер
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
+
+
+//модальное окно с видео
+var modal = document.getElementById('modalVideo');
+var btn = document.getElementById('btnVideo');
+var span = document.getElementsByClassName("close")[0];
+var video = document.getElementById('video');
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  video.play();
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+  video.pause();
+  video.currentTime = 0;
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
